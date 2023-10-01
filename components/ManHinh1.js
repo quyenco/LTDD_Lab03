0faef93e8df3cd34b002da51aca48656cf414451
dang co <dangco@gmail.com>
2023-10-01 23:23:31 +0700

    Create ManHinh3

--- components/ManHinh1.js
+++ components/ManHinh1.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View , Button, Alert, TouchableOpacity} from 'react-native';
+import { StyleSheet, Text, View ,  TouchableOpacity} from 'react-native';
 
 export default function ManHinh1() {
   return (
